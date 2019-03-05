b507d1d7209f8076d9f2f4d1d9f0b4fe1dfe2223
Huynh Duc Khoan <huynhduckhoan@gmail.com>
2019-03-05 13:34:27 +0700

    UPdate gulpfile for only index.pug

--- GULP_PUG/gulpfile.js
+++ GULP_PUG/gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp'),
 
 // run this task by typing in gulp pug in CLI
 gulp.task('pug', function () {
-    return gulp.src('*.pug')
+    return gulp.src('index.pug')
         .pipe(pug({
             pretty: true
         })) // pipe to pug plugin
